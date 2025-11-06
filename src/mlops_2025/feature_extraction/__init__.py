8738c05f92692ef6871611dd50a6dab36c78050d
anthonyayli <anthonyayli1@gmail.com>
2025-11-06 16:24:38 +0000

    commiting all necessary files for training_models

--- src/mlops_2025/feature_extraction/__init__.py
+++ src/mlops_2025/feature_extraction/__init__.py
@@ -3,10 +3,12 @@
 from mlops_2025.feature_extraction.base_feature_extractor import BaseFeatureExtractor
 from mlops_2025.feature_extraction.num_cat_transformer import NumCatTransformer
 from mlops_2025.feature_extraction.bins_transformer import BinsTransformer
+from mlops_2025.feature_extraction.titanic_feature_extractor import TitanicFeatureExtractor
 
 __all__ = [
     "BaseFeatureExtractor",
     "NumCatTransformer",
     "BinsTransformer",
+    "TitanicFeatureExtractor",
 ]
 
